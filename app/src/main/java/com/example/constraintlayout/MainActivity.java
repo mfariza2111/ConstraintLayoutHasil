36f2a0d3903921b16f82e57e62c5ee22c4745416
mfariza2111 <farizamuhammad310@gmail.com>
2021-03-09 15:41:44 +0700

    activity2

--- app/src/main/java/com/example/constraintlayout/MainActivity.java
+++ app/src/main/java/com/example/constraintlayout/MainActivity.java
@@ -2,13 +2,49 @@ package com.example.constraintlayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnLogin;
+
+    EditText edemail, edpassword;
+
+    String nama, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLogin=findViewById(R.id.btSignIn);
+
+        edemail=findViewById(R.id.edEmail);
+
+        edpassword=findViewById(R.id.edPassword);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                nama = edemail.getText().toString();
+                password = edpassword.getText().toString();
+
+                if(edemail.getText().toString().equals("admin@mail.com")&&edpassword.getText().toString().equals("123")) {
+                    Toast t = Toast.makeText(getApplicationContext(),
+                            "LOGIN SUKSES || email : " + nama + " password : " + password + "", Toast.LENGTH_LONG);
+                    t.show();
+                }
+                else{
+                    Toast.makeText((getApplicationContext()), "Username atau Password Anda salah",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
